d45731499b3b9b14ebae192c955f54e8a189ee79
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-09-16 01:33:51 -0700

    attempt

--- .travis/add_signing_key.sh
+++ .travis/add_signing_key.sh
@@ -5,8 +5,6 @@ security create-keychain -p travis ios-build.keychain
 wget https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
 security import AppleWWDRCA.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import certificate.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-echo "removeme!"
-echo 
-security import certificate.p12  -k  ~/Library/Keychains/ios-build.keychain  -P  -T /usr/bin/codesign
+security import certificate.p12  -k  ~/Library/Keychains/ios-build.keychain  -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
