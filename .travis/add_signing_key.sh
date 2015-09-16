24e7bfe436eec87cc0d6ce9e847e126ce9a1e4c2
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-09-16 01:00:41 -0700

    signing identity

---
+++ .travis/add_signing_key.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+pushd .travis/
+tar xvf secrets.tar
+security create-keychain -p travis ios-build.keychain
+wget https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
+security import AppleWWDRCA.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import certificate.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import certificate.p12  -k  ~/Library/Keychains/ios-build.keychain  -P  -T /usr/bin/codesign
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
