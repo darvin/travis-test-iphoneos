1d9e517c3c657fd2393459360e30e65089add9e2
Sergey Klimov <sergey.v.klimov@gmail.com>
2015-09-16 01:52:45 -0700

    unlocking keychain

--- .travis/add_signing_key.sh
+++ .travis/add_signing_key.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 pushd .travis/
 tar xvf secrets.tar
-security create-keychain -p travis ios-build.keychain
+KEY_CHAIN=ios-build.keychain
+security create-keychain -p travis $KEY_CHAIN
+security default-keychain -s $KEY_CHAIN
+security unlock-keychain -p travis $KEY_CHAIN
+security set-keychain-settings -t 3600 -u $KEY_CHAIN
+
 wget https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
-security import AppleWWDRCA.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import certificate.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import certificate.p12  -k  ~/Library/Keychains/ios-build.keychain  -P $KEY_PASSWORD -T /usr/bin/codesign
+security import AppleWWDRCA.cer  -k  ~/Library/Keychains/$KEY_CHAIN -T /usr/bin/codesign
+security import certificate.cer  -k  ~/Library/Keychains/$KEY_CHAIN -T /usr/bin/codesign
+echo "PASSWORD: " $KEY_PASSWORD
+security import certificate.p12  -k  ~/Library/Keychains/$KEY_CHAIN  -P $KEY_PASSWORD -T /usr/bin/codesign
+echo $?
+echo "certificate key imported"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
